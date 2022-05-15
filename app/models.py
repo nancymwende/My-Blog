4826a38e676fc6cf9b76988fb5ede9cde00031bc
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-15 14:21:32 +0300

    ft:create  a class user & pass Model arg. to connect to database.

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from . import db, login_manager
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Interger,primary_key = True)
+    username= db.Column(db.String(255))
+    
+    
+    
+    
+    
+    def __repr__(self):
+        return f'User {self.username}'
