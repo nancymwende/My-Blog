ddec1c1018fba2072d11c8138bf0e2a7974444ad
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-15 10:34:59 +0300

    ft:initialise bootstrap,sqlalchemy extensions.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+import sqlalchemy
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+
+
+db = SQLAlchemy
+bootstrap = Bootstrap
 
 #Initialising the apllication
 
@@ -8,6 +16,8 @@ def create_app(config_name):
     #creating the app configurations
     app.config.from_object(config_options[config_name])      
     
+    #initializing flask extensions
+    db.init_app(app)
     
     #registering the blueprint
     from .main import main as main_blueprint
