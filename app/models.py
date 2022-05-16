7d9d73b04064dc1ec52c791497d3d3823233f560
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-16 20:57:25 +0300

    ft:create  class comment for new user to write their comments.

--- app/models.py
+++ app/models.py
@@ -1,14 +1,21 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Interger,primary_key = True)
-    username= db.Column(db.String(255))
-    pass_secure = db.Column(db.String(255))
-    
-    
-    
+@login_manager.request_loader
+def load_user(user_id):
+        return User.query.get(int(user_id))
+        
+class User(UserMixin, db.Model):
+        __tablename__ = 'users'
+        
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(255))
+        email = db.Column(db.String(255), unique=True, index=True)
+        bio = db.Column(db.String(255))
+        profile_pic_path = db.Column(db.String())
+        password_secure = db.Column(db.String(255))
 
 @property
 def password(self):
@@ -18,13 +25,50 @@ def password(self):
 def password(self, password):
         self.pass_secure = generate_password_hash(password)
 
-def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
-    
-    
-    
-    
-    
-    
+def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
 def __repr__(self):
         return f'User {self.username}'
+        
+        
+class Quote:
+        '''
+        quotes class to define quotes objects
+        '''
+def __init__(self,author,quote,permalink):
+        self.author = author
+        self.quote = quote
+        self.permalink = permalink 
+        
+        
+class Post(db.Model):
+        _tablename_ = 'posts'
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(255))
+        category = db.Column(db.String)
+        content = db.Column(db.Text)
+        date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+        user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+def save_post(self):
+        db.session.add(self)
+        db.session.commit()
+
+def _repr_(self):
+        return f'Post {self.title}'
+        
+
+class Comment(db.Model):
+        __tablename__ = 'comments'
+        id = db.Column(db.Integer, primary_key=True)
+        comment = db.Column(db.String(255))
+        user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+        blog_id = db.Column(db.Integer, db.ForeignKey("blog.id"))
+
+
+def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+
