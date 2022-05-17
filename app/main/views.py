d04dc371d842bd2595edeedeed8c1eba948729fc
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-18 02:52:28 +0300

    fix:"write flash using the correct spelling"

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,64 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort,flash
+from ..requests import get_random_quotes
 from . import main
-from flask_login import login_required
+from ..models import User,Post,Comment,Blog
+from flask_login import login_required, current_user
+from .. import db, photos
+from .forms import UpdateProfile,CommentForm
 
 @main.route('/')
 def index():
+    
     '''
     view root function that returns render_template
     '''
-    title = 'Welcome to my Blog!'
+    quote = get_random_quotes()
+    return render_template('index.html', quote= quote)
     
-    return render_template('index.html',title = title)
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    all_blogs = Blog.get_current_blog(current_user.id).order_by(Blog.date_posted.desc()).all()
 
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)    
+    
+    
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.profile', uname=user.username))
+    return render_template('profile/update.html', form=form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+        return redirect(url_for('main.profile',uname=uname))
+@main.route('/comment', methods=['GET', 'POST'])
+@login_required
+def add_comment():
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(name=form.name.data)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Comment added successfully.')
+        return redirect(url_for('.index'))
+    return render_template('comments.html', form=form)    
